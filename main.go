c709164c1aae23b9a7f75024f4b03470fb7290d4
siddarth@pascalcase.com <siddarthg0910@gmail.com>
2025-08-19 23:13:20 +0530

    read from a conn and raw tcp server is donee

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"os"
+	"net"
 )
 
 func getLinesChannel(f io.ReadCloser) <-chan string {
@@ -41,12 +41,19 @@ func getLinesChannel(f io.ReadCloser) <-chan string {
 }
 
 func main() {
-	f, err := os.Open("messages.txt")
+	listener, err := net.Listen("tcp", ":42069")
 	if err != nil {
-		log.Fatal("error", "error", err)
+		log.Fatalf("error: %s\n", err.Error())
 	}
-	lines := getLinesChannel(f)
-	for line := range lines {
-        fmt.Printf("read: %s\n", line)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalf("error: %s\n", err)
+		}
+
+		for line := range getLinesChannel(conn) {
+			fmt.Printf("read: %s\n", line)
+		}
 	}
 }
