3533f217f1bb814e0429b3a00b36a38bec06b9d4
siddarth@pascalcase.com <siddarthg0910@gmail.com>
2025-08-19 13:01:04 +0530

    use goroutine to read and print data

--- main.go
+++ main.go
@@ -3,30 +3,50 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+			str += string(data)
+		}
+
+		if len(str) != 0 {
+			out <- str
+		}
+	}()
+
+	return out
+}
+
 func main() {
-	s, err := os.Open("messages.txt")
+	f, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal("error", "error", err)
 	}
-
-	str := ""
-	for {
-		data := make([]byte, 8)
-		n, err := s.Read(data)
-		if err != nil {
-			break
-		}
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i])
-			data = data[i+1:]
-			fmt.Printf("read: %s\n", str)
-			str = ""
-		}
-		str += string(data)
+	lines := getLinesChannel(f)
+	for line := range lines {
+        fmt.Printf("read: %s\n", line)
 	}
 }
