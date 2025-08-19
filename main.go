5aec2f051a04f3bfd3856957573f4a2d69e92edb
siddarth@pascalcase.com <siddarthg0910@gmail.com>
2025-08-19 12:59:43 +0530

    read 8 bytes but print line by line

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -12,12 +13,20 @@ func main() {
 		log.Fatal("error", "error", err)
 	}
 
+	str := ""
 	for {
 		data := make([]byte, 8)
 		n, err := s.Read(data)
 		if err != nil {
 			break
 		}
-		fmt.Printf("read: %s\n", string(data[:n]))
+		data = data[:n]
+		if i := bytes.IndexByte(data, '\n'); i != -1 {
+			str += string(data[:i])
+			data = data[i+1:]
+			fmt.Printf("read: %s\n", str)
+			str = ""
+		}
+		str += string(data)
 	}
 }
